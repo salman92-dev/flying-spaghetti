e20b5179cff2b7c99f6a2d5f293d6d61a065ae01
salman92-dev <124353689+salman92-dev@users.noreply.github.com>
2025-07-01 16:08:14 +0500

    Delete flyinh-spegheti directory

--- flyinh-spegheti/assets/javascript/script.js
+++
@@ -1,7 +0,0 @@
-const navbarToggle = document.querySelector(".navbar-toggle");
-const navbarLinks = document.querySelector(".center-menu");
-
-navbarToggle.addEventListener("click", () => {
-  navbarToggle.classList.toggle("active");
-  navbarLinks.classList.toggle("active");
-});
